32bc40acdfdd613c3871cdd761f97214d34058f5
etienne <etienne.doux@gmail.com>
2021-10-04 14:55:30 +0200

    DEV-9: correction of behat test

--- tests/Behat/features/bootstrap/FeatureContext.php
+++ tests/Behat/features/bootstrap/FeatureContext.php
@@ -52,12 +52,6 @@ class FeatureContext extends MinkContext implements Context
      */
     public function iCreateATask()
     {
-        $this->session->visit('http://localhost:8000/tasks/create');
-        $page = $this->session->getPage();
-        $page->fillField('task[title]', 'new behat task');
-        $page->fillField('task[content]', 'new task');
-        $button = $page->find('css', '.btn-success');
-        $button->press();
         $em = $this->kernel->getContainer()->get('doctrine')->getManager();
         $user = $em->getRepository(User::class)->findOneBy(['username' => 'user']);
         $task = new Task();
